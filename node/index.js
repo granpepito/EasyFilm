32f4e16b038eb4c5d639706466c568212147316c
granpepito <alexandre.goncalvesm@gmail.com>
2019-04-09 11:24:32 +0200

    Instruction cote back-end

--- node/index.js
+++ node/index.js
@@ -20,14 +20,17 @@ con.connect(function(err){
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+//App sur le port 4000
 app.listen(4000, function(){
   console.log('Example app listening on port 4000')
 })
 
+//homepage
 app.get('/', function(req,res){
   res.sendFile(__dirname + '/file.html');
 })
 
+//Afficher tous les films
 app.get('/films', function(req,res){
   con.query('SELECT * FROM films', (err,results) => {
     if (err)
@@ -36,6 +39,7 @@ app.get('/films', function(req,res){
   });
 });
 
+//Afficher les salles
 app.get('/salles', function(req,res){
   con.query('SELECT * FROM salles', (err,results) => {
     if (err)
@@ -44,6 +48,8 @@ app.get('/salles', function(req,res){
   })
 })
 
+//Afficher toutes les séances
+//Un peu inutiles, mieux vaut afficher toutes les séances qui n'ont pas encore été effectuées voire ne pas afficher du tout toutes les séances
 app.get('/seances', function(req,res){
   con.query('SELECT * FROM seance_projection', (err,results) => {
     if (err)
@@ -51,3 +57,31 @@ app.get('/seances', function(req,res){
     res.end(JSON.stringify(results))
   })
 })
+
+//Chercher les films encore programmés
+
+//Chercher les films qui ne sont plus programméswitch
+
+//Chercher un film (selon le titre / utilisation de mots-clés)
+
+//Chercher un film (selon le réalisateur)
+
+//Chercher un film (selon l'année de sortie)
+
+//Chercher un film selon
+
+//Chercher les salles affectées
+
+//
+
+//Chercher les séances à un jour donné
+
+//Chercher les séances à un jour donné, à une heure donnée
+
+//Chercher les séances à une heure donnée
+
+//Ajouter un film
+//Est-ce que ajouter un film implique aussi d'ajouter un nombre d'entrées prévisionnel dès l'ajout?
+//Programmer une séance / ou ajoute un nombre d'entrées prévisionnel
+
+//Vendre un billet
